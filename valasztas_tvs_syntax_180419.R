3726f9dec9e6576323fdb3dfe778b3f5d245bc84
hmg7 <38558897+hmg7@users.noreply.github.com>
2018-04-20 13:25:21 +0200

    Add files via upload

---
+++ valasztas_tvs_syntax_180419.R
@@ -0,0 +1,21 @@
+#FIDESZ LISTÁS EREDMÉNYÉNEK TÖBBVÁLTOZÓS ELEMZÉSE JÁRÁSI ADATOK ALAPJÁN
+#Hajdu Miklós, G7.hu
+
+library(readxl)
+library(MASS)
+library(betas)
+
+
+df <- read_excel("valasztas_tvs_input_180419.xlsx", sheet = "data_readin")
+
+#Budapestet kihagyjuk - feltételezésünk szerint itt más összefüggések várhatók, mint országosan
+df <- df[ which(substr(df$jnev,1,8)!='Budapest'), ]
+
+#Régió változó faktorként történő beállítása, hogy kontrollálhassunk rá a modellben
+df$regio.f <- factor(df$regio)
+is.factor(df$regio.f)
+
+#Robust regression
+model <- lmRob(fidesz_arany ~ isk + jov + vagyon + vallalk + mnrata + kor + kozmunk + regio.f, data = df)
+betas.lmr(model)
+summary(model)
